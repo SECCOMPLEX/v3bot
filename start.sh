c46c2421127fcf2de5454a587f7d2b48db092576
SECCOMPLEX <95067541+SECCOMPLEX@users.noreply.github.com>
2022-12-03 22:48:05 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/SECCOMPLEX/v3bot.git /v3bot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /v3bot
 fi
-cd /DQ-The-File-Donor
+cd /v3bot
 pip3 install -U -r requirements.txt
-echo "Starting DQ-The-File-Donor...."
+echo "Starting v3bot...."
 python3 bot.py
